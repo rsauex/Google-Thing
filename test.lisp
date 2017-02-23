03644e7e838c9b79b72cc133eb8b83ecd10cc349
Danylo Fedorov <fedorough@gmail.com>
2017-02-23 21:36:56 +0200

    @ functions

--- test.lisp
+++ test.lisp
@@ -1,3 +1,11 @@
+(defpackage #:net
+  (:use #:cl)
+  (:export #:with-net
+           #:@set-cache-size
+           #:@add-video
+           #:@add-endpoint
+           #:@add-request))
+
 (defvar *net*)
 
 (defmacro with-net (net &body body)
@@ -5,7 +13,7 @@
      ,@body))
 
 (defstruct connection
-  endpoint
+  endpoint-id
   latency
   cache-id) ;; if cache-id = t, connection is to data center
 
@@ -16,25 +24,34 @@
 (defstruct cache
   connection)
 
-(defstruct videos
-  endpoint-requests
+(defstruct video
+  endpoint-requests ; alist
   total-requests
   size)
 
 (defstruct net
   (endpoints-ht (make-hash-table))
   (caches-ht (make-hash-table))
-  (videos-ht (make-hash-table)))
+  (videos-ht (make-hash-table))
+  cache-size)
+
+(defun @set-cache-size (size)
+  (setf (net-cache-size *net*) size))
 
 (defun @add-video (id size)
-  (setf (gethash id (net-videos-ht *net*)) (make-videos :size size)))
+  (setf (gethash id (net-videos-ht *net*)) (make-video :size size :total-requests 0 :endpoint-requests nil)))
 
-;; (defun @add-endpoint (id latency cache-ids-list)
-;;   (setf (gethash id ())))
+(defun @add-endpoint (id data-center-latency cache-latency-list) ; list of conses: alist (cache-id . latency)
+  (let ((connections-list `((,(make-connection :endpoint-id id :latency data-center-latency :cache-id t)))))
+    (map nil (lambda (el) ; el is cons of (cache-id . latency)
+               (let ((connection (make-connection :endpoint-id id :cache-id (car el) :latency (cdr el))))
+                 (push connection connections-list)
+                 (setf (gethash (car el) (net-caches-ht *net*)) (make-cache :connection connection))))
+         cache-latency-list)
+    (setf (gethash id (net-endpoints-ht *net*)) (make-endpoint :connections connections-list))))
 
-;; (defun parse-input-file (file)
-;;   (with-open-file (stream file :direction :input)
-;;     (let ((first-line (read-line stream)))
-;;       )
-;;     ;; (do (line (read-line stream :eof-error-p nil :eof-value :eof) (read-line stream :eof-error-p nil :eof-value :eof)))
-;;     ))
+(defun @add-request (video-id endpoint-id requests-num)
+  (setf (gethash video-id (net-videos-ht *net*)) (make-video :size (video-size (gethash video-id (net-videos-ht *net*)))
+                                                             :endpoint-requests (push (cons endpoint-id requests-num)
+                                                                                      (video-endpoint-requests (gethash video-id (net-videos-ht *net*))))
+                                                             :total-requests (+ (video-total-requests (net-videos-ht *net*))))))
