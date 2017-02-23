8ba6c483cffa83123f28c900413b5d6ee22bd960
Danylo Fedorov <fedorough@gmail.com>
2017-02-23 21:46:12 +0200

    fixed

--- test.lisp
+++ test.lisp
@@ -54,4 +54,4 @@
   (setf (gethash video-id (net-videos-ht *net*)) (make-video :size (video-size (gethash video-id (net-videos-ht *net*)))
                                                              :endpoint-requests (push (cons endpoint-id requests-num)
                                                                                       (video-endpoint-requests (gethash video-id (net-videos-ht *net*))))
-                                                             :total-requests (+ (video-total-requests (net-videos-ht *net*))))))
+                                                             :total-requests (+ (video-total-requests (gethash video-id (net-videos-ht *net*)))))))
