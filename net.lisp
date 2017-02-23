2f7ea7adc237ba67cf69a14e0d3be89296689ade
Danylo Fedorov <fedorough@gmail.com>
2017-02-23 22:11:22 +0200

    smal fixes

--- net.lisp
+++ net.lisp
@@ -45,19 +45,22 @@
   (setf (gethash id (net-videos-ht *net*)) (make-video :size size :total-requests 0 :endpoint-requests nil)))
 
 (defun @add-endpoint (id data-center-latency cache-latency-list) ; list of conses: alist (cache-id . latency)
-  (let ((connections-list `((,(make-connection :endpoint-id id :latency data-center-latency :cache-id t)))))
-    (map nil (lambda (el) ; el is cons of (cache-id . latency)
-               (let ((connection (make-connection :endpoint-id id :cache-id (car el) :latency (cdr el))))
-                 (push connection connections-list)
-                 (setf (gethash (car el) (net-caches-ht *net*)) (make-cache :connection connection))))
-         cache-latency-list)
-    (setf (gethash id (net-endpoints-ht *net*)) (make-endpoint :connections connections-list))))
+  (when cache-latency-list
+    (let ((connections-list `(,(make-connection :endpoint-id id :latency data-center-latency :cache-id t))))
+      (map nil (lambda (el) ; el is cons of (cache-id . latency)
+                 (let ((connection (make-connection :endpoint-id id :cache-id (car el) :latency (cdr el))))
+                   (push connection connections-list)
+                   (setf (gethash (car el) (net-caches-ht *net*)) (make-cache :connection connection))))
+           cache-latency-list)
+      (setf (gethash id (net-endpoints-ht *net*)) (make-endpoint :connections connections-list)))))
 
 (defun @add-request (video-id endpoint-id requests-num)
   (setf (gethash video-id (net-videos-ht *net*)) (make-video :size (video-size (gethash video-id (net-videos-ht *net*)))
                                                              :endpoint-requests (push (cons endpoint-id requests-num)
                                                                                       (video-endpoint-requests (gethash video-id (net-videos-ht *net*))))
-                                                             :total-requests (+ (video-total-requests (gethash video-id (net-videos-ht *net*)))))))
+                                                             :total-requests (+ (video-total-requests (gethash video-id (net-videos-ht *net*))) requests-num)))
+  (setf (gethash endpoint-id (net-endpoints-ht *net*)) (make-endpoint :connections (endpoint-connections (gethash endpoint-id (net-endpoints-ht *net*)))
+                                                                      :vid-req (cons video-id requests-num))))
 
 (defun pretty-print (net)
   (with-net net
